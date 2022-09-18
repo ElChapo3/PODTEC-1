8ae346ad5ad51873fe5bca65cf675c33409d8b44
Gabriel <urubuvilaozin10@outlook.com>
2022-09-18 17:03:19 -0300

    consegui faze o carrossel caralho

---
+++ JS/script.js
@@ -0,0 +1,21 @@
+window.addEventListener('scroll', function () {
+    var header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 0);
+});
+
+let slides = document.querySelectorAll('.slide-container');
+let index = 0;
+
+function next() {
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function prev() {
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
+
+setInterval(next, 7000);
