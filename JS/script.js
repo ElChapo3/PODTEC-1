4e92d564339509aeda9a1825cf7ceb3b71def2d8
Gabriel <urubuvilaozin10@outlook.com>
2022-09-25 00:30:08 -0300

    free

--- JS/script.js
+++ JS/script.js
@@ -4,25 +4,6 @@ window.addEventListener('scroll', function () {
     header.classList.toggle('sticky', window.scrollY > 0);
 });
 
-//CARROUSSEL
-
-let slides = document.querySelectorAll('.slide-container');
-let index = 0;
-
-function next() {
-    slides[index].classList.remove('active');
-    index = (index + 1) % slides.length;
-    slides[index].classList.add('active');
-}
-
-function prev() {
-    slides[index].classList.remove('active');
-    index = (index - 1 + slides.length) % slides.length;
-    slides[index].classList.add('active');
-}
-
-setInterval(next, 7000);
-
 
 //VIDEO
 
@@ -53,3 +34,25 @@ btns.forEach((btn, i) => {
         sliderNav(i);
     });
 });
+
+
+//CARROUSSEL
+
+let slides = document.querySelectorAll('.slide-container');
+let index = 0;
+
+function next() {
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
+}
+
+function prev() {
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
+}
+
+setInterval(next, 7000);
+
+
