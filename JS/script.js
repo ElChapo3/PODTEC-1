4e63b64395417a08a27a85b998ff59aca1da137f
Gabriel <urubuvilaozin10@outlook.com>
2022-10-05 17:42:01 -0300

    commit

--- JS/script.js
+++ JS/script.js
@@ -55,4 +55,20 @@ function prev() {
 
 setInterval(next, 7000);
 
+//ANIMAÇÃO
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
+})
+
+const hiddenElements = document.querySelectorAll('.hidden')
+hiddenElements.forEach((el) => observer.observe(el))
+
 
